8ea3313827b07ddf57f5f3d83b81000e9c7f7e8e
Jaehyun Lee <myleje@gmail.com>
2019-12-29 14:39:21 +0900

    .

--- Assets/02.Scripts/MoveCtrl.cs
+++ Assets/02.Scripts/MoveCtrl.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class MoveCtrl : MonoBehaviour
 {
+    public enum MoveType
+    {
+        WAY_POINT,
+        LOOK_AT
+    }
+
+    public MoveType moveType = MoveType.WAY_POINT; //이동 방식을 결정할 변수
     public Transform[] points;
 
     void Start()
