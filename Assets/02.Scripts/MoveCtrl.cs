05058fe8809affb733f28e698caf2140f4c55963
Jaehyun Lee <myleje@gmail.com>
2019-12-29 14:41:57 +0900

    .

--- Assets/02.Scripts/MoveCtrl.cs
+++ Assets/02.Scripts/MoveCtrl.cs
@@ -13,6 +13,9 @@ public class MoveCtrl : MonoBehaviour
     public MoveType moveType = MoveType.WAY_POINT; //이동 방식을 결정할 변수
     public Transform[] points;
 
+    public float speed = 1.5f;
+    public float damping = 3.0f;  //회전 계수, 민감도
+
     void Start()
     {
         GameObject wayPointGroup = GameObject.Find("WayPointGroup");
@@ -25,6 +28,27 @@ public class MoveCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (moveType)
+        {
+            case MoveType.WAY_POINT:
+                MoveWayPoint();
+                break;
+            
+            case MoveType.LOOK_AT:
+                MoveLookAt();
+                break;
+        }
+    }
+
+    void MoveWayPoint()
+    {
+
+    }
+
+    void MoveLookAt()
+    {
         
     }
+
+
 }
