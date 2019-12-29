5d5b15af5068dae9e231da0582e615b5ac44654e
Jaehyun Lee <myleje@gmail.com>
2019-12-29 14:57:07 +0900

    .

--- Assets/02.Scripts/MoveCtrl.cs
+++ Assets/02.Scripts/MoveCtrl.cs
@@ -16,6 +16,8 @@ public class MoveCtrl : MonoBehaviour
     public float speed = 1.5f;
     public float damping = 3.0f;  //회전 계수, 민감도
 
+    public int nextIdx = 1;
+
     void Start()
     {
         GameObject wayPointGroup = GameObject.Find("WayPointGroup");
@@ -42,7 +44,13 @@ public class MoveCtrl : MonoBehaviour
 
     void MoveWayPoint()
     {
-
+        //벡터의 뺄셈연산을 통해서 방향 벡터을 산출
+        Vector3 dir = points[nextIdx].position - transform.position; 
+        //방향벡터의 쿼터니언타입의 각도를 계산
+        Quaternion rot = Quaternion.LookRotation(dir);
+        //구체선형보간 함수를 이용해 부드럽게 회전
+        transform.rotation = Quaternion.Slerp (transform.rotation, rot, Time.deltaTime * damping);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 
     void MoveLookAt()
