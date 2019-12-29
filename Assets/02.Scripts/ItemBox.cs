d206c56c8ae497fca30d4296bd8c906199f6a7fb
Jaehyun Lee <myleje@gmail.com>
2019-12-29 16:26:24 +0900

    .

---
+++ Assets/02.Scripts/ItemBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+
+    public void OnLookAt(bool isLook)
+    {
+        Debug.Log("isLook=" + isLook);
+    }
+
+}
